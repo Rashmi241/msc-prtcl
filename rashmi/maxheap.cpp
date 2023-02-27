bffaea37d4ffaf2b9a3c6c3a370a53ce39f55772
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2023-02-27 06:45:20 +0530

    Add files via upload

---
+++ rashmi/maxheap.cpp
@@ -0,0 +1,48 @@
+#include<iosream>
+using namespace std;
+void heapsub(int a[],int n,int i)
+{ int largest =i;
+int l=2*i+1;
+int r=2*i+2;
+if(r<n&&a[r]>a[largest])
+largest =r;
+if(l<n&&a[l]>a[largest])
+largest =l;
+if(largest!=i)
+{
+swap(a[i],a[largest];
+heapsub(a,n,largest);
+}
+}
+void heapsort(int a[],int n)
+{
+for(int i=n/2-1;i>=0;i--)
+heapsub(a,n,y);
+for(int i=n-1;i>=0;i--)
+{
+swap(a[0],a[i]);
+int temp=a[i];
+for(int j=i;j<n;j--)
+a[j]=a[j+1];
+a[n-1]=temp;
+heapsub(a,l,0);
+}
+}
+void display(int a[],int n)
+{
+for (int i=0;i<n;i++)
+cout<<a[i]<<" ";
+cout<<endl;
+}
+int main()
+{
+int n,a[10];
+cout<<"enter the number of element hespsub:"<<endl;
+dispay(a,n);
+cout<<"enter the  element hespsub:"<<endl;
+cin>>a[i];
+heapsort(a,n);
+cout<<"after heapsub the sorted array is"<<endl;
+dispay(a,n);
+return 0;
+}
---
+++ rashmi/minsort.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+void heapsub(int a[],int n,int i)
+{ int smallest =i;
+int l=2*i+1;
+int r=2*i+2;
+if(r<n&&a[r]<a[smallest])
+smallest=r;
+if(l<n&&a[l]<a[smallest])
+smallest =l;
+if(smallest!=i)
+{
+swap(a[i],a[smallest]);
+heapsub(a,n,smallest);
+}
+}
+void heapsort(int a[],int n)
+{
+for(int i= n/2-1;i>=0;i--)
+heapsub(a,n,i);
+for(int i= n-1;i>=0;i--)
+{
+    int l;
+swap(a[0],a[i]);
+int temp=a[i];
+for(int j=i;j<n;j--)
+a[j]=a[j+1];
+a[n-1]=temp;
+heapsub(a,l,0);
+}
+}
+void display(int a[],int n)
+{
+for (int i=0;i<n;i++)
+cout<<a[i]<<" ";
+cout<<endl;
+}
+int main()
+{
+int n,a[10];
+cout<<"enter the number of element hespsub:"<<endl;
+cin>>n;
+cout<<"enter the  element hespsub:"<<endl;
+for(int i=0;i<n;i++)
+cin>>a[i];
+heapsort(a,n);
+cout<<"after heapsub the sorted array is"<<endl;
+display(a,n);
+return 0;
+}
