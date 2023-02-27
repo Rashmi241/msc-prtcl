8eaef8a9795f2c3139eb4b453e29066a377688a7
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2023-02-27 14:05:14 +0530

    edited

--- rashmi/maxheap.cpp
+++ rashmi/maxheap.cpp
@@ -1,4 +1,4 @@
-#include<iosream>
+#include<iostream>
 using namespace std;
 void heapsub(int a[],int n,int i)
 { int largest =i;
@@ -10,27 +10,27 @@ if(l<n&&a[l]>a[largest])
 largest =l;
 if(largest!=i)
 {
-swap(a[i],a[largest];
+swap(a[i],a[largest]);
 heapsub(a,n,largest);
 }
 }
 void heapsort(int a[],int n)
 {
 for(int i=n/2-1;i>=0;i--)
-heapsub(a,n,y);
+heapsub(a,n,i);
 for(int i=n-1;i>=0;i--)
 {
 swap(a[0],a[i]);
 int temp=a[i];
-for(int j=i;j<n;j--)
+for(int j=i;j<n;j++)
 a[j]=a[j+1];
 a[n-1]=temp;
-heapsub(a,l,0);
+heapsub(a,i,0);
 }
 }
 void display(int a[],int n)
 {
-for (int i=0;i<n;i++)
+for (int i=0;i<n;++i)
 cout<<a[i]<<" ";
 cout<<endl;
 }
@@ -38,11 +38,12 @@ int main()
 {
 int n,a[10];
 cout<<"enter the number of element hespsub:"<<endl;
-dispay(a,n);
-cout<<"enter the  element hespsub:"<<endl;
+cin>>n;
+cout<<"enter the  element heaspsub:"<<endl;
+for(int i=0;i<n;i++)
 cin>>a[i];
 heapsort(a,n);
 cout<<"after heapsub the sorted array is"<<endl;
-dispay(a,n);
+display(a,n);
 return 0;
 }
