c1bb561615e52a04a8be9f8eb3f308487ba68423
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2023-03-02 15:08:08 +0530

    edited array operation 2/3/23

--- rashmics/insrtdlt.cs
+++ rashmics/insrtdlt.cs
@@ -1,57 +1,45 @@
 using System;
-namespace insert 
-{ 
- class start
-  {
- public static void Main(string[] args)
+class arrayop
 {
-int i,n,pos,num; 
-int[] arr=new int[5];
-Console.WriteLine("enter the size of array");
+public static void Main()
+{
+int[] arr=new int[8];
+int i,n,ele,pos;
+Console.WriteLine("enter the size of array");  
 n=int.Parse(Console.ReadLine());
-Console.WriteLine("enter the array elements:");
+Console.WriteLine("enter the array elements");
 for(i=0;i<n;i++)
 {
 arr[i]=int.Parse(Console.ReadLine());
 }
-Console.WriteLine("enter the insert element");
-num=int.Parse(Console.ReadLine());
-Console.WriteLine("enter the position");
-pos=int.parse(Console.ReadLine());
-if(pos>n+1||pos<=0)
-{
-Console.WriteLine("invalid");
-}
-else
-{
-for(i=n;i>=pos;i--)
-{
-a[i]=a[i-1];
-}
-a[pos-1]=num;
-n++;
-}
-Console.WriteLine("array after insertion:");
-for(i=0;i<n;i++)
-Console.WriteLine(a[i]+"  ");
-Console.WriteLine(); 
-Console.Writeline("enter the position to delete");
+Console.WriteLine("enter the element to insert:");
+ele=int.Parse(Console.ReadLine());
+Console.WriteLine("enter the element pos to insert");
 pos=int.Parse(Console.ReadLine());
-for(i=pos;i=n-1;i++)
- {
- a[i]=a[i+1];
- }
-Console.WriteLine("array after deletion");
+n+=1;
+for(i=n;i>=pos;i--)
+arr[i+1]=arr[i];
+arr[pos]=ele;
+Console.WriteLine("array after insertion");
 for(i=0;i<n;i++)
- {
- Console.WriteLine("a[i]+" ");
- }
+{
+Console.WriteLine(arr[i]);
+}
+Console.WriteLine("enter the element to delete:");
+ele=int.Parse(Console.ReadLine());
+//Console.WriteLine("enter the element pos to delete");
+//pos=int.Parse(Console.ReadLine());
+for(i=pos-1;i<n-1;i++)
+
+//ele=arr[pos];
+arr[i]=arr[i+1];
+//ele=arr[pos];
+n-=1;
+
+Console.WriteLine("array after deletion");
+for(i=0;i<n-1;i++)
+{
+Console.WriteLine(arr[i]);
 }
 }
 }
- 
-
-
-
-
-
