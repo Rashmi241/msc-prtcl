17f8aa53c4de6dcb5b5a165c3ba19409fd58c9e5
Rashmi <98954270+Rashmi241@users.noreply.github.com>
2023-04-28 11:20:15 +0530

    28/4/23

---
+++ python/io.py
@@ -0,0 +1,13 @@
+print('enter your name')
+name=input("name:")
+print(name)
+print(type(name))
+
+#output
+"""
+pc55@pc55:~/rashmi/python$ python3 io.py
+enter your name
+name:radha
+radha
+<class 'str'>
+"""
---
+++ python/vrbl.py
@@ -0,0 +1,20 @@
+t='tommy'
+x=23
+i=12.45
+print(i)
+print(x)
+print(t) 
+t='man'
+print(t)
+a='good'
+print(t+a)
+
+"""
+output:
+pc55@pc55:~$ python3 vrbl.py
+12.45
+23
+tommy
+man
+mangood
+"""
